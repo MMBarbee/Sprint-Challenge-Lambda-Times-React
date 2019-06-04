fd1e58700f2b43bb1e62280c8cf663e5d56bb622
myasia barbee <myasiabarbee@icloud.com>
2019-06-04 12:29:39 -0400

    Content + Tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -6,10 +7,11 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={`tab${props.selectedTab === props.tab ? " active-tab": ""}`}
+      onClick={tab => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectTabHandler(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
@@ -18,5 +20,9 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
-
+Tab.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+}
 export default Tab;
